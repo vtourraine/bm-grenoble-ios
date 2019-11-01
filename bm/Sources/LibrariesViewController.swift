6f28ac3630e79c03d6919a7039968980d4514238
Vincent Tourraine <me@vtourraine.net>
2019-11-01 18:14:57 +0100

    Add libraries map

--- bm/Sources/LibrariesViewController.swift
+++ bm/Sources/LibrariesViewController.swift
@@ -7,15 +7,51 @@
 //
 
 import UIKit
+import MapKit
 
-class LibrariesViewController: UITableViewController {
+class LibrariesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
 
     let libraries = Libraries.loadCityLibraries()
+
+    @IBOutlet var tableView: UITableView?
+    @IBOutlet var mapView: MKMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.configureCustomAppearance()
+
+        let regionRadius: CLLocationDistance = 7000
+        let center = CLLocationCoordinate2D(latitude: 45.1793553, longitude: 5.724542)
+        let coordinateRegion = MKCoordinateRegion(center: center, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView?.setRegion(coordinateRegion, animated: true)
+
+        if let libraries = libraries?.libraries {
+            let annotations = libraries.map { (library) -> MKPointAnnotation in
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = library.location()
+                annotation.title = library.name
+                return annotation
+            }
+
+            mapView?.addAnnotations(annotations)
+        }
+    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "Id")
+        view.displayPriority = .required
+        return view
+    }
+
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let annotation = view.annotation,
+            let libraries = libraries,
+            let index = libraries.libraries.firstIndex(where: { $0.name == annotation.title }) else {
+                return
+        }
+
+        tableView?.scrollToRow(at: IndexPath(row: index, section: 0), at: .top, animated: true)
     }
 
     // MARK: - Actions
@@ -26,11 +62,11 @@ class LibrariesViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return libraries?.libraries.count ?? 0
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         if let library = libraries?.libraries[indexPath.row] {
             cell.textLabel?.text = library.name
@@ -43,7 +79,7 @@ class LibrariesViewController: UITableViewController {
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let indexPath = tableView.indexPathForSelectedRow,
+        guard let tableView = tableView, let indexPath = tableView.indexPathForSelectedRow,
             let libraryViewController = segue.destination as? LibraryViewController,
             let library = libraries?.libraries[indexPath.row] else {
                 return
